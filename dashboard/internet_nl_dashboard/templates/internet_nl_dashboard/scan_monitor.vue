16f52c2a95a124c9b65b251726b75a8d3e4c6b9d
stitch1 <elger.jonker@gmail.com>
2019-03-26 19:04:41 +0100

    Added auto refresh

--- dashboard/internet_nl_dashboard/templates/internet_nl_dashboard/scan_monitor.vue
+++ dashboard/internet_nl_dashboard/templates/internet_nl_dashboard/scan_monitor.vue
@@ -1,15 +1,16 @@
 {% verbatim %}
 <template type="x-template" id="scan_monitor_template">
     <div>
-        <h3>{{ $t("upload.recent_uploads.title") }}</h3>
-        <p>{{ $t("upload.recent_uploads.intro") }}</p>
+        <h3>{{ $t("scan_monitor.title") }}</h3>
+        <p>{{ $t("scan_monitor.intro") }}</p>
+        <div class='auto_refresh'>{{ $t("auto_refresh.refresh_happening_in") }} <span v-html="current_step_inverted"></span>{{ $t("auto_refresh.units") }} <a @click="reload_now()"> ({{ $t("auto_refresh.refresh_now") }})</a></div>
         <table v-if="scans">
             <thead>
                 <tr>
-                    <th>{{ $t("upload.recent_uploads.date") }}</th>
-                    <th>{{ $t("upload.recent_uploads.filename") }}</th>
-                    <th>{{ $t("upload.recent_uploads.filesize") }}</th>
-                    <th>{{ $t("upload.recent_uploads.status") }}</th>
+                    <th>{{ $t("scan_monitor.id") }}</th>
+                    <th>{{ $t("scan_monitor.started_on") }}</th>
+                    <th>{{ $t("scan_monitor.finished_on") }}</th>
+                    <th>{{ $t("scan_monitor.message") }}</th>
                 </tr>
             </thead>
             <tbody>
@@ -21,7 +22,7 @@
                 </tr>
             </tbody>
         </table>
-        <span v-if="!scans.length">{{ $t("upload.recent_uploads.no_uploads") }}</span>
+        <span v-if="!scans.length">{{ $t("scan_monitor.no_scans") }}</span>
     </div>
 </template>
 {% endverbatim %}
@@ -30,32 +31,38 @@
 
 const messages = {
     en: {
-        upload: {
-            recent_uploads: {
-                title: 'Recent uploads',
-                intro: 'This list shows your recent uploads. The status messages give an impression of what has been ' +
-                    'created or added. If something went wrong, the status contains hints on what to do next.' +
-                    'if your upload was not successful',
-                date: 'Date',
-                filename: 'Filename',
-                filesize: 'Size',
-                status: 'Status',
-                no_uploads: 'No files uploaded.',
-            }
+        scan_monitor: {
+            title: 'Scan monitor',
+            intro: 'All scans that have happened for this account are displayed here. It gives an insight into how ' +
+                'recent the most current information is. It can also help you with comparisons to select the ideal ' +
+                'scan.',
+            id: '#',
+            started_on: 'Started on',
+            finished_on: 'Finished on',
+            message: 'Status',
+            no_scans: 'No scans have been performed yet.',
+        },
+        auto_refresh: {
+            refresh_happening_in: 'Auto refresh in:',
+            units: 's',
+            refresh_now: 'refresh now'
         }
     },
     nl: {
-        upload: {
-            recent_uploads: {
-                title: 'Recent geupload',
-                intro: 'Deze lijst geeft de meest recente uploads weer. De status berichten geven aan wat er is toegevoegd. ' +
-                    'Mocht er iets verkeerd zijn gegaan bij het uploaden, dan is hier advies te vinden over wat te verbeteren.',
-                date: 'Datum',
-                filename: 'Bestand',
-                filesize: 'Grootte',
-                status: 'Status (in het Engels)',
-                no_uploads: 'Nog geen bestanden geüpload.',
-            }
+        scan_monitor: {
+            title: 'Scan monitor',
+            intro: 'Alle scans die zijn uitgevoerd voor dit account staan hier. Het geeft een overzicht in hoe recent ' +
+                'de data is. Het geeft ook inzicht in of de meest recente scan al is afgerond.',
+            id: '#',
+            started_on: 'Begonnen',
+            finished_on: 'Klaar',
+            message: 'Status',
+            no_scans: 'Nog geen scans uitgevoerd.',
+        },
+        auto_refresh: {
+            refresh_happening_in: 'Lijst wordt ververst over:',
+            units: 's',
+            refresh_now: 'ververs nu'
         }
     }
 };
@@ -66,12 +73,61 @@ const i18n = new VueI18n({
     messages,
 });
 
+
+const autoreload_mixin = {
+    /*
+    * Autoreloads the 'load' methods after every N seconds.
+    * */
+    data: {
+        // 60 * 1000ms
+        refresh_time: 60 * 1000,
+
+        // Counts down until the next refresh. Allows to add a visualisation on this timer.
+        countdown_percentage: 0,
+
+        // countdown every ms (more means more UI changes, slower site)
+        countdown_refresh_time: 1000,
+
+        //
+        countdown_steps: 0,
+
+        //
+        current_step: 1
+    },
+    methods: {
+        init_auto_refresh: function () {
+            setInterval(this.lower_countdown, this.countdown_refresh_time);
+            this.countdown_steps = this.refresh_time /  this.countdown_refresh_time;
+        },
+        lower_countdown: function(){
+            if (100 / (this.countdown_steps / this.current_step) >= 100) {
+                this.reload_now();
+            } else {
+                this.current_step += 1;
+            }
+        },
+        reload_now: function(){
+            this.current_step = 0;
+            this.load();
+        }
+    },
+    mounted: function(){
+        this.init_auto_refresh();
+    },
+    computed: {
+        current_step_inverted: function(){
+            return this.countdown_steps - this.current_step;
+        }
+    }
+};
+
+
 vueUpload = new Vue({
     i18n,
     name: 'scan_monitor',
     el: '#scan_monitor',
     template: '#scan_monitor_template',
-    mixins: [humanize_mixin],
+    mixins: [humanize_mixin, autoreload_mixin],
     data: {
         scans: [],
     },
