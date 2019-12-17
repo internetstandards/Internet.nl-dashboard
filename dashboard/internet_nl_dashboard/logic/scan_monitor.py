680bc3b6e6fb38c2c124e042f8d13e7fafe76c0d
stitch1 <elger.jonker@gmail.com>
2019-12-17 11:53:52 +0100

    Use state instead in monitor for finished on

--- dashboard/internet_nl_dashboard/logic/scan_monitor.py
+++ dashboard/internet_nl_dashboard/logic/scan_monitor.py
@@ -22,15 +22,12 @@ def get_running_scans(account: Account) -> List:
         # it's probably fine but far from ideal.
 
         last_report_id = None
-        if scan.scan.finished_on:
+        # Finished means also report created, mail sent, etc.
+        if scan.state == "finished":
             # first report within the next day
 
             last_report = UrlListReport.objects.all().filter(
                 urllist=scan.urllist,
-                # We can expect a report to be ready 24 hours after the scan has finished (?)
-                # Otherwise maybe just get the latest scan for the urllist?
-                # reports are stored at the last possible moment of the day. So every day there is at max 1 report.
-                # If a scan if finished at exactly 00:00, the next report will be stored at 23:59:59 ...
                 at_when__lte=scan.scan.finished_on + timedelta(hours=24),
                 at_when__gte=scan.scan.finished_on
             ).order_by('-id').only('id').first()
@@ -38,7 +35,7 @@ def get_running_scans(account: Account) -> List:
             if last_report:
                 last_report_id = last_report.id
 
-        if scan.scan.finished_on:
+        if scan.state == "finished":
             runtime = scan.scan.finished_on - scan.scan.started_on
         else:
             runtime = timezone.now() - scan.scan.started_on
