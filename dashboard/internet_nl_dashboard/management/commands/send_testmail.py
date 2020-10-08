94bdfa641c5260e8587a8d552b4f5aa253c2fd32
stitch1 <elger.jonker@gmail.com>
2020-10-08 14:17:08 +0200

    Fix sending testmail from the right account

--- dashboard/internet_nl_dashboard/management/commands/send_testmail.py
+++ dashboard/internet_nl_dashboard/management/commands/send_testmail.py
@@ -5,7 +5,7 @@ from django_mail_admin import mail, models
 class Command(BaseCommand):
     def handle(self, *args, **options):
         mail.send(
-            'noreply@internet.nl',
+            'noreply@dashboard.internet.nl',
             'elger@internetcleanup.foundation',
             subject='Dashboard test e-mail',
             message='This is a test email from the dashboard.',
