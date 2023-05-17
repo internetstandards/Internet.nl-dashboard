3df4311df4e8d955c41caf47937a4dbd849faf26
stitch1 <elger.jonker@gmail.com>
2023-05-17 15:17:49 +0200

    fix #436

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -122,6 +122,11 @@ MIDDLEWARE = [
 
     # Two factor Auth
     'django_otp.middleware.OTPMiddleware',
+
+    # https://docs.djangoproject.com/en/4.2/ref/middleware/#django.middleware.gzip.GZipMiddleware
+    # This middleware should be placed before any other middleware that need to read or write the response body so
+    # that compression happens afterward.
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
